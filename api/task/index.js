cee37a2045b7a7b77d473496460492ddbcb728b2
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-03-29 15:29:31 -0500

    feat: add the compose middleware

--- api/task/index.js
+++ api/task/index.js
@@ -7,14 +7,14 @@ const {
   handlerCreateTask,
   handlerUpdateTask,
 } = require('./task.controller');
-const { isAuthenticated } = require('../../auth/auth.service');
+const { isAuthenticated, hasRole } = require('../../auth/auth.service');
 
 const router = Router();
 
 router.get('/', handlerAllTask);
 router.get('/:id', handlerOneTask);
-router.delete('/:id', handlerDeleteTask);
-router.post('/', isAuthenticated, handlerCreateTask);
+router.delete('/:id', hasRole(['admin']), handlerDeleteTask);
+router.post('/', isAuthenticated(), handlerCreateTask);
 router.patch('/:id', handlerUpdateTask);
 
 module.exports = router;
--- auth/auth.service.js
+++ auth/auth.service.js
@@ -1,4 +1,5 @@
 const jsonwebtoken = require('jsonwebtoken');
+const compose = require('composable-middleware');
 
 const { getUserByEmail } = require('../api/user/user.service');
 
@@ -17,35 +18,62 @@ async function validateToken(token) {
   }
 }
 
-async function isAuthenticated(req, res, next) {
-  // 1. req.headers -> authorization
-  const authHeader = req.headers.authorization;
-  // 2. If (authHeader)
-  if (!authHeader) {
-    return res.status(401).end();
-  }
-  // 3. split para obtener el token
-  const [, token] = authHeader.split(' ');
-  // 4. validar el token
-  const payload = await validateToken(token);
+/**
+ * Attaches the user object to the request if authenticated
+ * Otherwise returns 403
+ */
+function isAuthenticated() {
+  return compose().use(
+    async (req, res, next) => {
+      // 1. req.headers -> authorization
+      const authHeader = req.headers.authorization;
+      // 2. If (authHeader)
+      if (!authHeader) {
+        return res.status(401).end();
+      }
+      // 3. split para obtener el token
+      const [, token] = authHeader.split(' ');
+      // 4. validar el token
+      const payload = await validateToken(token);
 
-  // 5. if token falsy -> decir q no esta authori
-  if (!payload) {
-    return res.status(401).end();
-  }
+      // 5. if token falsy -> decir q no esta authori
+      if (!payload) {
+        return res.status(401).end();
+      }
 
-  // 6. buscar el usuario por el email del payload del token
-  const user = await getUserByEmail(payload.email);
+      // 6. buscar el usuario por el email del payload del token
+      const user = await getUserByEmail(payload.email);
 
-  if (!user) {
-    return res.status(401).end();
-  }
+      if (!user) {
+        return res.status(401).end();
+      }
 
-  // 7. agregar ese usuario al req.user
-  req.user = user;
-  // 8. siga al siguiente middleware next()
-  next();
-  return null;
+      // 7. agregar ese usuario al req.user
+      req.user = user;
+      // 8. siga al siguiente middleware next()
+      next();
+      return null;
+    },
+  );
+}
+
+/**
+ * Checks if the user role meets the minimum requirements of the route
+ */
+function hasRole(allowRoles = []) {
+  // 1. verficar que este autenticado
+  return compose()
+    .use(isAuthenticated())
+    .use((req, res, next) => {
+      const { role } = req.user;
+
+      if (!allowRoles.includes(role)) {
+        return res.status(403).send('Forbidden');
+      }
+
+      next();
+      return null;
+    });
 }
 
 /**
@@ -63,5 +91,6 @@ function signToken(payload) {
 
 module.exports = {
   isAuthenticated,
+  hasRole,
   signToken,
 };
